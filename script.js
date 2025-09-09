29a93884467fe6d006d421df078474a3cf5e8d14
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-09 14:24:45 +0200

    stampa in colsole la bici più leggera

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ console.log("provaaaa");
 
 //inizializzo l'arry di oggetti
 const bikes = [
-    {nome: "Alltrail" ,peso : 6800},
-    {nome: "Winora" ,peso : 6200},
-    {nome: "Brera" ,peso : 6100},
-    {nome: "Bottecchia" ,peso : 6900},
+    {name: "Alltrail" ,weigth : 6800},
+    {name: "Winora" ,weigth : 6200},
+    {name: "Brera" ,weigth : 6100},
+    {name: "Bottecchia" ,weigth : 6900},
 ]
 
 //rimnando a schermo la bici più leggera
+let lighterBike = bikes[0];
+for (let i = 0; i < bikes.length; i++) {
+    if (bikes[i].weigth < bikes[0].weigth){
+        lighterBike = bikes[i];
+    }
+}
+
+console.log("La bici che pesa di meno è: " +lighterBike.name, (lighterBike.weigth/1000) + "kg");
