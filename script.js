424bc96fecbbff157d810627eb35fe6f9b73ce71
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-09 14:17:23 +0200

    inizializzo l'array di oggetti

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("provaaaa");
+
+//snack 1
+
+//inizializzo l'arry di oggetti
+const bikes = [
+    {nome: "Alltrail" ,peso : 6800},
+    {nome: "Winora" ,peso : 6200},
+    {nome: "Brera" ,peso : 6100},
+    {nome: "Bottecchia" ,peso : 6900},
+]
+
+//rimnando a schermo la bici più leggera
